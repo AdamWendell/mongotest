3133e2993b0ccac8cf981fb6e76ff2fb993f2d52
AdamWendell <Adam@websmith.se>
2016-02-27 22:23:08 +0100

    Started the project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "new-cap": 0,
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true
+        }
+    }
+};
